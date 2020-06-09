e5a5d309cc7b2d0f43cb1a2e04f5d13eccc0f687
hyansuper <hyansuper@foxmail.com>
2020-06-09 21:33:24 +0800

    add mic volumn control

--- cozmars/microphone.py
+++ cozmars/microphone.py
@@ -3,19 +3,37 @@ import numpy as np
 from . import error, util
 
 class MicrophoneOutputStream(util.SyncAsyncRPCStream):
+    def __init__(self, async_stream, loop, dtype):
+        util.SyncAsyncRPCStream.__init__(self, async_stream, loop)
+        self._dtype = dtype
     def _decode(self, data):
-        return np.frombuffer(data, dtype='int16')
+        return np.frombuffer(data, dtype=self._dtype)
 
 class Microphone(util.StreamComponent):
-    def __init__(self, robot, samplerate, q_size):
+    def __init__(self, robot, samplerate, dtype, q_size):
         util.StreamComponent.__init__(self, robot)
         self._q_size = q_size
         self._samplerate = samplerate
+        self._dtype = dtype
 
     def _get_rpc(self):
-        rpc = self.rpc.microphone(self.samplerate, q_size=self._q_size)
-        self._output_stream = MicrophoneOutputStream(rpc.response_stream, None if self._mode=='aio' else self._loop)
+        rpc = self.rpc.microphone(self.samplerate, self.dtype, q_size=self._q_size)
+        self._output_stream = MicrophoneOutputStream(rpc.response_stream, None if self._mode=='aio' else self._loop, self.dtype)
         return rpc
+
+    @property
+    def dtype(self):
+        return self._dtype
+
+    @dtype.setter
+    def dtype(self, dt):
+        if not self.closed:
+            raise error.CozmarsError('Cannot set dtype while microphone is recording')
+        self._dtype = dt
+
+    @property
+    def channels(self):
+        return 1
 
     @property
     def samplerate(self):
@@ -26,6 +44,10 @@ class Microphone(util.StreamComponent):
         if not self.closed:
             raise error.CozmarsError('Cannot set samplerate while microphone is recording')
         self._samplerate = sr
+
+    @util.mode(property_type='setter')
+    async def volumn(self, *args):
+        return await self.rpc.microphone_volumn(*args)
 
     @property
     def output_stream(self):
--- examples/record_sound.py
+++ examples/record_sound.py
@@ -1,11 +1,15 @@
-# record sound from microphone for 3 seconds and save to file
+# record sound from microphone for 5 seconds and save to file
 # requires soundfile module
 
 import cozmars
 import soundfile as sf
 
 with cozmars.Robot(ip='192.168.1.102') as robot:
-    with robot.microphone as mic, sf.SoundFile('sound.wav', mode='x', samplerate=mic.samplerate, channels=1, subtype='PCM_24') as file:
+
+    # set record volumn to 100%, volumn changes will be save even after reboot
+    robot.microphone.volumn = 100
+
+    with robot.microphone as mic, sf.SoundFile('sound.wav', mode='x', samplerate=mic.samplerate, channels=mic.channels, subtype='PCM_24') as file:
 
         count = 0
         for buff in mic.output_stream:
@@ -13,5 +17,5 @@ with cozmars.Robot(ip='192.168.1.102') as robot:
 
             # every buffer is 0.1 second
             count += 1
-            if count == 30:
+            if count == 50:
                 break
