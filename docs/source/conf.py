ae2b04eee99c3dbd17a9c36572e3e47552619881
hyansuper <hyansuper@foxmail.com>
2020-07-04 19:34:47 +0800

    add sphinx-extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,10 +52,7 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx_automodapi.automodapi',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',]
+extensions = ['sphinx.ext.autodoc',]
 
 pygments_style = 'sphinx'
 autodoc_member_order = 'bysource'
